df702311b5df30eb15da6f30fddc93e9aa28f42c
lucacicada <100947658+lucacicada@users.noreply.github.com>
2025-10-29 21:18:25 +0100

    test: Update URL test cases

--- tests/url.test.ts
+++ tests/url.test.ts
@@ -4,14 +4,15 @@ import { fileURLToPath, pathToFileURL } from '../src/common/url'
 it.each([
   ['C:\\test', 'file:///C:/test'],
   ['C:\\test  ', 'file:///C:/test%20%20'],
+  ['C:\\test  a ', 'file:///C:/test  a%20'],
   ['C:\\test\\?', 'file:///C:/test/%3F'],
   ['C:\\test\\#', 'file:///C:/test/%23'],
   ['C:\\test\\%25', 'file:///C:/test/%2525'],
   ['C:\\test\\\n', 'file:///C:/test/%0A'],
   ['C:\\test\\\r', 'file:///C:/test/%0D'],
   ['C:\\test\\\t', 'file:///C:/test/%09'],
-  ['C:\\test\\a b c', 'file:///C:/test/a%20b%20c'],
-  ['C:\\test\\a b c#d?e%f', 'file:///C:/test/a%20b%20c%23d%3Fe%25f'],
+  ['C:\\test\\a b c', 'file:///C:/test/a b c'],
+  ['C:\\test\\a b c#d?e%f', 'file:///C:/test/a b c%23d%3Fe%25f'],
 ])('pathToFileURL("%s") -> "%s"', (a, expected) => {
   expect(pathToFileURL(a)).toBe(expected)
 })
